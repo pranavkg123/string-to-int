fd50d48b7992558576d91da5dff7d7d93413535a
pranavkg123 <75836381+pranavkg123@users.noreply.github.com>
2024-11-29 13:34:09 +0530

    Add files via upload

---
+++ string 2.py
@@ -0,0 +1,7 @@
+name = "pranav"
+if len(name) < 3:
+    print('The name must be at least three characters.')  # 'print' is now the built-in function
+elif len(name) > 50:
+    print('The name can be a maximum of 50 characters.')
+else:
+    print('The name looks good.')
